5498a8785f68c7a75c05adabaca6ffc094faca67
Nikhil Sharma <nikhil.sharma8398@gmail.com>
2019-01-29 12:39:09 +0530

    Added a few new test cases

--- Assignment3/PingHost.java
+++ Assignment3/PingHost.java
@@ -51,3 +51,52 @@ class RequestProcessor {
             requestCheck.pingTheAddress();
         }
 }
+
+/* 1. Enter web address:
+ *    google.co.in
+ *    Pinging 5 times...
+ *    PING google.co.in (172.217.163.131) 56(84) bytes of data.
+ *    64 bytes from maa05s04-in-f3.1e100.net (172.217.163.131): icmp_seq=1 ttl=52 time=47.9 ms
+ *    64 bytes from maa05s04-in-f3.1e100.net (172.217.163.131): icmp_seq=2 ttl=52 time=48.3 ms
+ *    64 bytes from maa05s04-in-f3.1e100.net (172.217.163.131): icmp_seq=3 ttl=52 time=48.3 ms
+ *    64 bytes from maa05s04-in-f3.1e100.net (172.217.163.131): icmp_seq=4 ttl=52 time=53.7 ms
+ *    64 bytes from maa05s04-in-f3.1e100.net (172.217.163.131): icmp_seq=5 ttl=52 time=48.2 ms
+ *
+ *    --- google.co.in ping statistics ---
+ *    5 packets transmitted, 5 received, 0% packet loss, time 4005ms
+ *    rtt min/avg/max/mdev = 47.907/49.338/53.775/2.237 ms
+ *
+ *    Median time taken to ping: 48.3 ms
+ *
+ * 2. Enter web address:
+ *    archive.org
+ *    Pinging 5 times...
+ *    PING archive.org (207.241.224.2) 56(84) bytes of data.
+ *    64 bytes from www.archive.org (207.241.224.2): icmp_seq=1 ttl=47 time=374 ms
+ *    64 bytes from www.archive.org (207.241.224.2): icmp_seq=2 ttl=47 time=294 ms
+ *    64 bytes from www.archive.org (207.241.224.2): icmp_seq=3 ttl=47 time=317 ms
+ *    64 bytes from www.archive.org (207.241.224.2): icmp_seq=4 ttl=47 time=340 ms
+ *    64 bytes from www.archive.org (207.241.224.2): icmp_seq=5 ttl=47 time=362 ms
+ *
+ *    --- archive.org ping statistics ---
+ *    5 packets transmitted, 5 received, 0% packet loss, time 4005ms
+ *    rtt min/avg/max/mdev = 294.700/337.925/374.730/29.179 ms
+ *
+ *    Median time taken to ping: 340.0 ms
+ *
+ * 3. Enter web address: 
+ *    github.com
+ *    Pinging 5 times...
+ *    PING github.com (192.30.253.113) 56(84) bytes of data.
+ *    64 bytes from lb-192-30-253-113-iad.github.com (192.30.253.113): icmp_seq=1 ttl=51 time=280 ms
+ *    64 bytes from lb-192-30-253-113-iad.github.com (192.30.253.113): icmp_seq=2 ttl=51 time=305 ms
+ *    64 bytes from lb-192-30-253-113-iad.github.com (192.30.253.113): icmp_seq=3 ttl=51 time=226 ms
+ *    64 bytes from lb-192-30-253-113-iad.github.com (192.30.253.113): icmp_seq=4 ttl=51 time=250 ms
+ *    64 bytes from lb-192-30-253-113-iad.github.com (192.30.253.113): icmp_seq=5 ttl=51 time=272 ms
+ *   
+ *    --- github.com ping statistics --- 
+ *    5 packets transmitted, 5 received, 0% packet loss, time 4001ms
+ *    rtt min/avg/max/mdev = 226.570/267.220/305.135/26.800 ms
+ *    
+ *    Median time taken to ping: 272.0 ms 
+ */
